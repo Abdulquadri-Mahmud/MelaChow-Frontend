575cbefbbb3b496f071b87a59ac84e8fbff4f003
AdeyemiCode <abdulquadrimahmud06@gmail.com>
2025-12-13 20:46:51 +0100

    global base url implemented

--- src/app/context/ApiContext.jsx
+++ src/app/context/ApiContext.jsx
@@ -5,7 +5,7 @@ const ApiContext = createContext({ baseUrl: ""});
 
 export const ApiProvider = ({ children }) => {
   // const baseUrl = "http://localhost:3001/api";
-  const baseUrl = "http://localhost:3001/api";
+  const baseUrl = "https://grub-dash-api.vercel.app/api";
 
   return (
     <ApiContext.Provider value={{ baseUrl }}>
