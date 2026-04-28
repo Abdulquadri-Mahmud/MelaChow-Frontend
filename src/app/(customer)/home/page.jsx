ed11357dc5d00506305c811f25f5ec34fcd48945
AdeyemiCode <abdulquadrimahmud06@gmail.com>
2026-04-28 11:39:43 +0100

    fix: wrap VendorList in Suspense to resolve Vercel prerender error

--- src/app/(customer)/home/page.jsx
+++ src/app/(customer)/home/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import CategoryList from "@/app/components/Home_Components/Category";
 import FeatureSlider from "@/app/components/Home_Components/FeatureSlider";
 import FoodList from "@/app/components/Home_Components/FoodList";
@@ -34,7 +34,9 @@ export default function HomePage() {
         <CategoryList />
         <PromoAnnouncementBanner />
         <div id="vendor-list-anchor">
-          <VendorList user={user} />
+          <Suspense fallback={null}>
+            <VendorList user={user} />
+          </Suspense>
         </div>
         {/* <TrendingFoods user={user} /> */}
         <div className="space-y-4">
