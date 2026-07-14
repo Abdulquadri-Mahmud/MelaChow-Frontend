6da2b3475378f792f8223374d09dedf772ae9e71
AdeyemiCode <abdulquadrimahmud06@gmail.com>
2026-07-14 02:43:53 +0100

    fix: portal combo modal to document body

--- src/app/components/modals/GlobalComboModal.jsx
+++ src/app/components/modals/GlobalComboModal.jsx
@@ -3,11 +3,18 @@
 import { useComboModalStore } from "@/app/store/comboModalStore";
 import ComboDetailsClient from "@/app/(customer)/combo-details/[comboId]/ComboDetailsClient";
 import { AnimatePresence } from "framer-motion";
+import { useSyncExternalStore } from "react";
+import { createPortal } from "react-dom";
+
+const subscribe = () => () => {};
 
 export default function GlobalComboModal() {
   const { isOpen, comboId, initialData, closeComboModal } = useComboModalStore();
+  const mounted = useSyncExternalStore(subscribe, () => true, () => false);
 
-  return (
+  if (!mounted) return null;
+
+  return createPortal(
     <AnimatePresence>
       {isOpen && comboId && (
         <ComboDetailsClient
@@ -18,6 +25,7 @@ export default function GlobalComboModal() {
           initialData={initialData}
         />
       )}
-    </AnimatePresence>
+    </AnimatePresence>,
+    document.body
   );
 }
